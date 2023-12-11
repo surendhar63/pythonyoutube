65c561aabfa1e0fdb5f58f43efdb3c08e811ff8d
surendhar63 <surendhar63808@gmail.com>
2023-12-11 17:21:39 +0530

    to branch

--- summa.py
+++ summa.py
@@ -1,1 +1,10 @@
-print("venna")
+while True:
+    try:
+        int_1=int(input("No1:"))
+        int_2=int(input("No2:"))
+        print(int_1*int_2)
+        break
+    except Exception:
+        print("Numbers Only")
+        
+
